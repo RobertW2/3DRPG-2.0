67752058195ed5a7a227ac6169f8fd5ee9ac5f06
RobertW2 <bobbyweeden2@gmail.com>
2021-05-20 16:27:00 +1000

    Inventory System

---
+++ REAL 3D RPG/Assets/Scripts/Inverntory/Inventory.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+namespace InventorySystem
+{
+    public class Inventory : MonoBehaviour
+    {
+        [SerializeField] private List<Item> inventory = new List<Item>();
+        [SerializeField] private bool showIMGUIInventory = true;
+        private Item selectedItem = null;
+
+        #region CanvasInventory
+        [SerializeField] private Button ButtonPrefab;
+        [SerializeField] private GameObject InventoryGameObject;
+        [SerializeField] private GameObject InventoryContent;
+        [SerializeField] private GameObject FilterContent;
+        #endregion
+
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.I))
+            {
+                InventoryGameObject.SetActive(true);
+                DisplayItemsCanvas();
+            }
+        }
+
+        private void DisplayItemsCanvas()
+        {
+            for(int i = 0; i < inventory.Count; i++)
+            {
+                if(inventory[i].Type.ToString() == sortType || sortType == "All")
+                {
+                    Button buttonGO = Instantiate<Button>(ButtonPrefab, InventoryContent.transform);
+                    Text buttonText = buttonGO.GetComponentInChildren<Text>();
+                    buttonGO.name = inventory[i].Name + " button";
+                    buttonText.text = inventory[i].Name;
+                }
+            }
+        }
+
+        private void Display()
+        {
+            scrollPosition = GUI.BeginScrollView(new Rect(0, 40, Screen.width, Screen.height - 40), scrollPosition,
+ 
+                new Rect(0, 0, 0, inventory.Count * 30),
+                false,
+                true);
+            int count = 0;
+            for(int i =0; i < inventory.Count; i++)
+            {
+                if(inventory[i].Type.ToString() == sortType || sortType == "All")
+                {
+                    if(GUI.Button(new Rect(30, 0 + (count * 30), 200, 30), inventory[i].Name))
+                    {
+                        selectedItem = inventory[i];
+                    }
+                    count++;
+                }
+            }
+            GUI.EndScrollView();
+        }
+
+
+
+
+        #region Display Inventory
+        private Vector2 scrollPosition;
+        private string sortType = "All";
+        #endregion
+
+
+        private void OnGUI()
+        {
+            if(showIMGUIInventory)
+            {
+                GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
+
+                List<string> itemTypes = new List<string>(Enum.GetNames(typeof(Item.ItemType)));
+                itemTypes.Insert(0, "All");
+
+                for(int i = 0; i < itemTypes.Count; i++)
+                {
+                    if (GUI.Button(new Rect(
+                        (Screen.width / itemTypes.Count) * i
+                        , 10
+                        , Screen.width / itemTypes.Count
+                        , 20), itemTypes[i]))
+                    {
+                        sortType = itemTypes[i];
+                    }
+                }
+                Display();
+                if(selectedItem != null)
+                {
+                    DisplayDelectedItem();
+                }
+            }
+        }
+
+        private void DisplayDelectedItem()
+        {
+            GUI.Box(new Rect(Screen.width / 4, Screen.height / 3,
+                Screen.width / 5, Screen.height / 5), selectedItem.Icon);
+
+            GUI.Box(new Rect(Screen.width / 4, (Screen.height / 3) + (Screen.height / 5),
+                Screen.width / 7, Screen.height / 15), selectedItem.Name);
+
+            GUI.Box(new Rect(Screen.width / 4, (Screen.height / 3) + (Screen.height / 3),
+                Screen.width / 5, Screen.height / 5), selectedItem.Description +
+                "\nValue: " + selectedItem.Value +
+                "\nAmount: " + selectedItem.Amount);
+        }
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+
+    }
+}
---
+++ REAL 3D RPG/Assets/Scripts/Inverntory/Item.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InventorySystem
+{
+    [System.Serializable]
+    public class Item
+    {
+
+        public enum ItemType
+        {
+            Food,
+            Weapon,
+            Apparel,
+            Crafting,
+            Ingredients,
+            Potions,
+            Scrolls,
+            Quest,
+            Money
+
+        }
+
+
+        #region Private variables
+        [SerializeField] private string name;  // Item's ID
+        [SerializeField] private string description; // Item's ID
+        [SerializeField] private int value; // Item's ID
+        [SerializeField] private int amount; // Item's ID
+        [SerializeField] private Texture2D icon; // Item's ID
+        [SerializeField] private GameObject mesh; // Item's ID
+        [SerializeField] private ItemType type; // Item's ID
+        [SerializeField] private int damage; // Item's ID
+        [SerializeField] private int armour; // Item's ID
+        [SerializeField] private int heal; // Item's ID
+        #endregion
+
+        #region Public properties
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public int Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public int Amount
+        {
+            get { return amount; }
+            set { Amount = value; }
+        }
+
+        public Texture2D Icon
+        {
+            get { return icon; }
+            set { icon = value; }
+        }
+
+        public GameObject Mesh
+        {
+            get { return mesh; }
+            set { mesh = value; }
+        }
+
+        public ItemType Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        
+        public int Damage
+        {
+            get { return damage; }
+            set { damage = Mathf.Min(999,value); }
+        }
+        
+        public int Armour
+        {
+            get { return armour; }
+            set { armour = value; }
+        }
+
+        public int Heal
+        {
+            get { return heal; }
+            set { heal = value; }
+        }
+        #endregion
+
+        public void thisIsSomeWhereElse()
+        {
+            Item myItem = new Item();
+
+            Item anotherItem = new Item(myItem, 5);
+        }
+
+        public Item()
+        {
+
+        }
+
+        public Item(Item copyItem, int copyAmount)
+        {
+            Name            = copyItem.Name;
+            Description     = copyItem.Description;
+            Value           = copyItem.Value;
+            Amount          = copyItem.Amount;
+            Icon            = copyItem.Icon;
+            Mesh            = copyItem.Mesh;
+            Type            = copyItem.Type;
+            Damage          = copyItem.Damage;
+            Armour          = copyItem.Armour;
+            Heal            = copyItem.Heal;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
