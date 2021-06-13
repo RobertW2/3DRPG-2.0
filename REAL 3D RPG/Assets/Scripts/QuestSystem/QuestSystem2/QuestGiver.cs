345c182b326cdf153785a8a7df45977a8039f980
Bobby <69677528+RobertW2@users.noreply.github.com>
2021-06-13 23:09:07 +1000

    Started QuestSystem

---
+++ REAL 3D RPG/Assets/Scripts/QuestSystem/QuestSystem2/QuestGiver.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestGiver : MonoBehaviour
+{
+    public Quest quest;
+
+    public GameObject player;
+
+    public GameObject questWindow;
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI descriptionText;
+    public TextMeshProUGUI experienceText;
+    public TextMeshProUGUI goldText;
+ 
+
+    public void OpenQuestWindow()
+    {
+     /*   if(Input.GetKeyDown("j"))
+        {
+            gameObject.SetActive(true);
+        }
+     */
+
+        questWindow.SetActive(true);
+        titleText.text = quest.title;
+        descriptionText.text = quest.description;
+        experienceText.text = quest.experienceReward.ToString();
+        goldText.text = quest.goldReward.ToString();
+    }
+
+    
+}
