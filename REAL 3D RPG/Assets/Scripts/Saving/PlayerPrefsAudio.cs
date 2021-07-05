97e5c9eb5bb8a56adbf25dc3fff1df055744fb36
Bobby <69677528+RobertW2@users.noreply.github.com>
2021-07-05 12:40:26 +1000

    Added LoadingScreen and Audio PlayerPrefs

---
+++ REAL 3D RPG/Assets/Scripts/Saving/PlayerPrefsAudio.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   
+
+public class PlayerPrefsAudio : MonoBehaviour
+{
+    public Slider SoundEffects;
+    public Slider Music;
+
+    void Start()
+    {
+
+        SoundEffects.value = PlayerPrefs.GetFloat("OptionScore");
+        Music.value = PlayerPrefs.GetFloat("OptionLives");
+    }
+
+    void Update()
+    {
+        PlayerPrefs.SetFloat("OptionScore", SoundEffects.value);
+        PlayerPrefs.SetFloat("OptionLives", Music.value);
+
+    }
+}
